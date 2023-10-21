5928b702c31864368d6021f122c14fcbf5990138
caique325 <134814288+caique325@users.noreply.github.com>
2023-10-21 20:51:50 -0300

    Update forca.c

--- forca.c
+++ forca.c
@@ -5,22 +5,39 @@
 
 int main () {
 char palavra[8] = "MELANCIA";
-char chutes[8] = "MELANCIA";
+char chutes[8];
 char chute;
-int acertou = 0; 
+int tentativas = 0;
+int acabou = 0;
  
-
+do{
 
 for(int i = 0; i < strlen(palavra);i++){
-	if(chutes[i] == palavra[i])	{
+	
+	int achou  = 0;
+
+	for (int j = 0 ; j < tentativas;j++){
+		if(chutes[j] == palavra[i])	{
+			achou = 1;
+			break;	
+		}
+	}
+	
+	if(achou) {
 		printf("%c",palavra[i]);
-			
 	}else{
 		printf("_ ");
 	}
-	
-}	
+}
 
+printf("\n");
+char chute;
+scanf(" %c",&chute);	
+chutes[tentativas] = chute;
+tentativas++;
 
+acabou = 1;
+
+}while(acabou);
 
 }
